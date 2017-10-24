d987236d6ba7bc4d10b355beb04ac60f2ef9040c
barba <guido.lm@gmail.com>
2017-10-24 01:51:14 -0700

    adding tooltip to let people know that right clicking leads to scripting

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ToolbarItem from './ToolbarItem';
 import _ from 'lodash';
+import ReactTooltip from 'react-tooltip';
 
 export default (props) => (
-  <div style={props.style} className="toolbar">
+  <div data-tip="Right-click a script to edit its code" style={props.style} className="toolbar">
     {_.range(props.numberOfItems).map((i) => (
       <ToolbarItem
         key={i}
@@ -16,9 +17,17 @@ export default (props) => (
       />
     ))}
 
+    <ReactTooltip className="customTooltip" effect="solid" />
+
     <style jsx>{`
       .toolbar {
         display: flex;
+      }
+    `}</style>
+
+    <style jsx global>{`
+      .customTooltip {
+        font-family: monospace !important;
       }
     `}</style>
   </div>
