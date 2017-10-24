8a1ea0fd2802239b8b80903fda39a5a4d4b1f92a
barba <guido.lm@gmail.com>
2017-10-24 02:01:58 -0700

    making tooltip per-item

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -4,20 +4,21 @@ import _ from 'lodash';
 import ReactTooltip from 'react-tooltip';
 
 export default (props) => (
-  <div data-tip="Right-click a script to edit its code" style={props.style} className="toolbar">
+  <div style={props.style} className="toolbar">
     {_.range(props.numberOfItems).map((i) => (
-      <ToolbarItem
-        key={i}
-        selected={props.selectedIndex === i}
-        onClick={() => props.setSelected(i)}
-        onContextMenu={() => props.editCode(i)}
-        style={{marginRight: '2px'}}
-        hasCode={props.hasCode(i)}
-        text={props.getItemText(i)}
-      />
+      <div key={i} data-tip="Right-click to hack!">
+        <ToolbarItem
+          key={i}
+          selected={props.selectedIndex === i}
+          onClick={() => props.setSelected(i)}
+          onContextMenu={() => props.editCode(i)}
+          style={{marginRight: '2px'}}
+          hasCode={props.hasCode(i)}
+          text={props.getItemText(i)}
+        />
+        <ReactTooltip className="customTooltip" effect="solid" />
+      </div>
     ))}
-
-    <ReactTooltip className="customTooltip" effect="solid" />
 
     <style jsx>{`
       .toolbar {
